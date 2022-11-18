94d5c142f17bdf10637684d1e10854e0bb938a9b
Monica Crowe <mcrowe9194@gmail.com>
2022-11-17 21:11:07 -0600

    finished exercise

---
+++ DapperProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyDatabaseUsingDapper
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM products WHERE ProductID = @id", new {id});
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price, CategoryID = @catID, OnSale = @onSale, StockLevel = @stockLevel WHERE ProductID = @id", new {name = product.Name, price = product.Price, catID = product.CategoryID, onSale = product.OnSale, stockLevel = product.StockLevel, id = product.ProductID});
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,89 @@
-﻿namespace BestBuyDatabaseUsingDapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace BestBuyDatabaseUsingDapper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //EXERCISE 1! 
+
+            //step 1- done
+            //step 2- done
+            //step 3- done
+            //step 4- done
+            //step 5- done
+            //step 6 - done
+            //step 7 - done
+            //step 8 - done
+            //step 9 - done
+            //step 10 - done
+            //step 11 - done
+            //step 12 - done
+
+            //EXERCISE 2!
+            //step 1 - done
+            //step 2 - done
+            //step 3 - done
+            //step 4 - done 
+
+            //EXERCISE 3!
+            //No steps - just added the UpdateProduct method in the DapperProductRepository
+            //
             
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            #region Department Section
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var departmentRepo = new DapperDepartmentRepository(conn);
+
+            departmentRepo.InsertDepartment("Moni's New Department!");
+
+            var departments = departmentRepo.GetAllDepartments();
+
+            foreach (var department in departments)
+            {
+                Console.WriteLine(department.DepartmentID);
+                Console.WriteLine(department.Name);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            #endregion
+
+            var productRepository = new DapperProductRepository(conn);
+
+            var productToUpdate = productRepository.GetProduct(943);
+
+            productToUpdate.Name = "UPDATED!";
+            productToUpdate.Price = 10.99m;
+            productToUpdate.CategoryID = 10;
+            productToUpdate.StockLevel = 500;
+            productToUpdate.OnSale = false;
+
+            productRepository.UpdateProduct(productToUpdate);
+
+
+            var products = productRepository.GetAllProducts();
+
+            foreach(var product in products)
+            {
+                Console.WriteLine(product.ProductID);
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.CategoryID);
+                Console.WriteLine(product.OnSale);
+                Console.WriteLine(product.StockLevel);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
         }
     }
 }
